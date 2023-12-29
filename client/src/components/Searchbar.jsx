8eab8cfad4e95ebac78e5e7b3abde861054b2192
wwwwswe <rotey1016@gmail.com>
2023-12-29 18:41:47 +0900

    feat: Searcbar 미디어 쿼리 업데이트

--- client/src/components/Searchbar.jsx
+++ client/src/components/Searchbar.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { AiOutlineSearch } from "react-icons/ai";
-import { styled } from "styled-components";
+import styled from "styled-components";
 
 const SearchBar = ({ onSearch }) => {
   const [searchQuery, setSearchQuery] = useState("");
@@ -10,12 +10,10 @@ const SearchBar = ({ onSearch }) => {
   };
 
   const handleSearchClick = () => {
-    // 검색 버튼이 클릭될 때 검색 쿼리를 부모 컴포넌트로 전달
     onSearch(searchQuery);
   };
 
   const handleKeyPress = (e) => {
-    // Enter 키를 눌렀을 때도 검색 쿼리를 부모 컴포넌트로 전달
     if (e.key === "Enter") {
       onSearch(searchQuery);
     }
@@ -43,6 +41,21 @@ const SearchBarContainer = styled.div`
   background-color: #cbd1d4;
   padding: 8px;
   border-radius: 4px;
+
+  @media screen and (max-width: 768px) {
+    /* 모바일 화면 크기에 대한 스타일 */
+    padding: 6px;
+  }
+
+  @media screen and (min-width: 769px) and (max-width: 1024px) {
+    /* 테블릿 화면 크기에 대한 스타일 */
+    padding: 10px;
+  }
+
+  @media screen and (min-width: 1025px) {
+    /* 데스크탑 화면 크기에 대한 스타일 */
+    padding: 12px;
+  }
 `;
 
 const SearchIcon = styled.div`
@@ -63,6 +76,19 @@ const SearchInput = styled.input`
   margin-left: 8px;
   font-size: 16px;
   color: #333;
+  width: 100%;
+
+  @media screen and (min-width: 768px) {
+    /* 모바일 이상의 화면에서 input 너비 조정 */
+    /* 예시: 테블릿부터는 너비를 줄임 */
+    width: 80%;
+  }
+
+  @media screen and (min-width: 1025px) {
+    /* 데스크탑 이상의 화면에서 input 너비 조정 */
+    /* 예시: 데스크탑부터는 더 넓은 너비로 조정 */
+    width: 90%;
+  }
 `;
 
 export default SearchBar;
